277bfac665dee14c05da1349826b162bbb819c01
VIVEK KUMAR <vk0730267@gmail.com>
2025-06-17 21:43:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+  if (arr.length < 2) return -Infinity;
+
+  let first = -Infinity;
+  let second = -Infinity;
+
+  for (let num of arr) {
+    if (num > first) {
+      second = first;
+		
+      first = num;
+    } else if (num < first && num > second) {
+      second = num;
+    }
+  }
+
+  return (second === -Infinity) ? -Infinity : second;
+}
